015077c57ffcf9d2b757667d0909059a0fb405f7
Vinay Mourya <vmorya350@gmail.com>
2023-12-12 22:16:14 +0530

    sign up

---
+++ Database/db.js
@@ -0,0 +1,21 @@
+// username - vmourya350
+// password - UkeK13DBDoXTiYnj
+
+// const uri = 'mongodb+srv://vmourya350:UkeK13DBDoXTiYnj@cluster0.yur5rgf.mongodb.net/?retryWrites=true&w=majority';
+
+// db.js
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+        process.exit(1); // Exit with failure
+    }
+};
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // app.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const User = require('./models/userModel.js');
+
+//loading db
+require('dotenv').config();
+const connectDB = require('./Database/db');
+
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+connectDB();
+
+// check mongo schema
+// const newUser = new User({
+//   "username": 'sih',
+//   "email": 'sih@example.com',
+//   "password": '12345',
+//   "dob": new Date('1990-01-01'),
+// });
+app.post('./api/users', async (req, res) => {
+  try {
+    // Extract user data from the request body
+    const { username, email, password, dob } = req.body;
+
+    // Create a new user instance
+    const newUser = new User({
+      username,
+      email,
+      password,
+      dob,
+    });
+    console.log('Before saving user to the database');
+    // Save the user to the database
+    await newUser.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error(`Error creating user: ${error.message}`);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Save the user to the database
+// newUser.save()
+//   .then(() => {
+//     console.log('User saved to the database');
+//   })
+//   .catch((error) => {
+//     console.error(`Error saving user to the database: ${error.message}`);
+//   });
 
 // Serve static files from the 'public' directory
 // app.use(express.static('public'));
@@ -12,6 +61,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at http://localhost:${process.env.PORT}`);
 });
